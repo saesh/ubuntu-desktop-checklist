8f4eeac44c1de84c611d0d01fc3a333254b3102f
Sascha Hagedorn <mail@shagedorn.net>
2021-05-06 13:24:48 +0200

    Fix invalid syntax for tail

--- bin/ti.sh
+++ bin/ti.sh
@@ -2,7 +2,7 @@
 
 timeclockFile=${TIMELOG:-${HOME}/.${USER}.timeclock}
 
-if [[ -f "$timeclockFile" && "$(tail -1 -f "$timeclockFile" | cut -c 1)" == "i" ]]; then
+if [[ -f "$timeclockFile" && "$(tail -1 "$timeclockFile" | cut -c 1)" == "i" ]]; then
     to
 fi
 
